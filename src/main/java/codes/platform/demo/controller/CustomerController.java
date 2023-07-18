0c3f3dab928ae89eb440159d233a92cb462c6f49
Luan Ricardo Gazin <luan_gazin@yahoo.com.br>
2023-07-18 18:20:24 -0300

    Add logs

--- src/main/java/codes/platform/demo/controller/CustomerController.java
+++ src/main/java/codes/platform/demo/controller/CustomerController.java
@@ -2,6 +2,9 @@ package codes.platform.demo.controller;
 
 import codes.platform.demo.entity.Customer;
 import codes.platform.demo.repository.CustomerRepository;
+import java.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -9,22 +12,28 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
+  private final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
-    private final RestTemplate restTemplate;
-    private final CustomerRepository customerRepository;
+  private final RestTemplate restTemplate;
+  private final CustomerRepository customerRepository;
 
-    public CustomerController(RestTemplate restTemplate, CustomerRepository customerRepository) {
-        this.restTemplate = restTemplate;
-        this.customerRepository = customerRepository;
-    }
+  public CustomerController(RestTemplate restTemplate, CustomerRepository customerRepository) {
+    this.restTemplate = restTemplate;
+    this.customerRepository = customerRepository;
+  }
 
-    @PostMapping
-    public ResponseEntity<Customer> post(@RequestBody Customer customer) {
-        return ResponseEntity.ok(customerRepository.save(customer));
-    }
+  @PostMapping
+  public ResponseEntity<Customer> post(@RequestBody Customer customer) {
+    logger.info(String.format(String.format("Posting costumer %s", customer.getCustomerName())));
+    return ResponseEntity.ok(customerRepository.save(customer));
+  }
 
-    @GetMapping
-    public ResponseEntity<String> get() {
-        return restTemplate.getForEntity("http://app-exemple-platform-k8s-service-0.vkpr:8081/info", String.class);
-    }
+  @GetMapping
+  public ResponseEntity<String> get() {
+    logger.info("Calling k8s service /info");
+    ResponseEntity<String> forEntity = restTemplate.getForEntity("http://app-exemple-platform-k8s-service-0.vkpr:8081/info",
+        String.class);
+    logger.info(String.format("Response from k8s service /info: %s", forEntity.getBody()));
+    return forEntity;
+  }
 }
